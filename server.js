18aed7a5ff78d8548b0f4f71a9c87aa74b7d7757
Limani Ratnayake <lpw.ratnayake@gmail.com>
2024-02-17 23:13:37 +0530

    fix front-end

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.static(path.resolve(_dirname, "./public")));
+app.use(express.static(path.resolve(_dirname, "./client/dist")));
 
 app.get("/", (req, res) => {
   res.send("Hello world");
@@ -82,7 +82,7 @@ app.use("/api/v1/auth", authRouter);
 
 // publicly available the client folder
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+  res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
 });
 
 // resource not found middleware
