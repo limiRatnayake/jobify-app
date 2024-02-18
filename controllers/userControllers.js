3972cf79d19932aee93ff364b102e398f7c08d51
Limani Ratnayake <lpw.ratnayake@gmail.com>
2024-02-18 12:00:22 +0530

    fixed update user

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -3,6 +3,7 @@ import User from "../models/userModel.js";
 import Job from "../models/jobModel.js";
 import cloudinary from "cloudinary";
 import { promises as fs } from "fs"; //file system module
+import { formatImage } from "../middleware/multerMiddleware.js";
 
 export const getCurrentUser = async (req, res) => {
   const user = await User.findOne({ _id: req.user.userId });
@@ -16,12 +17,14 @@ export const getApplicationStats = async (req, res) => {
   res.status(StatusCodes.OK).json({ users, jobs });
 };
 
+// this update user with file memory upload
 export const updateUser = async (req, res) => {
   const newUser = { ...req.body };
   delete newUser.password;
   if (req.file) {
-    const response = await cloudinary.v2.uploader.upload(req.file.path);
-    await fs.unlink(req.file.path); //we wanna remove the public/upload files that added after submitting for cloud
+    const file = formatImage(req.file)
+    const response = await cloudinary.v2.uploader.upload(file);
+    
     newUser.avatar = response.secure_url;
     newUser.avatarPublicId = response.public_id;
   }
@@ -32,3 +35,21 @@ export const updateUser = async (req, res) => {
   }
   res.status(StatusCodes.CREATED).json({ msg: `update user` });
 };
+
+//This update user with file disk upload
+// export const updateUser = async (req, res) => {
+//   const newUser = { ...req.body };
+//   delete newUser.password;
+//   if (req.file) {
+//     const response = await cloudinary.v2.uploader.upload(req.file.path);
+//     // await fs.unlink(req.file.path); //we wanna remove the public/upload files that added after submitting for cloud - we used in disk storage setup
+//     newUser.avatar = response.secure_url;
+//     newUser.avatarPublicId = response.public_id;
+//   }
+//   const updateUser = await User.findByIdAndUpdate(req.user.userId, newUser);
+//   // check oldUser(updateUser has values before user get updated) has a avatar if so then delete the old avatar frm cloud
+//   if (req.file && updateUser.avatarPublicId) {
+//     await cloudinary.v2.uploader.destroy(updateUser.avatarPublicId);
+//   }
+//   res.status(StatusCodes.CREATED).json({ msg: `update user` });
+// };
--- middleware/multerMiddleware.js
+++ middleware/multerMiddleware.js
@@ -3,22 +3,36 @@
 It is commonly used for handling file uploads. Multer simplifies the process of accepting and storing files submitted 
 through HTTP requests by providing an easy-to-use API. It integrates seamlessly with Express.js and allows developers
  to define upload destinations, file size limits, and other configurations. it has disk storage and memory 
- storage we use disk storage here */
+ storage we use disk storage here. but with Render disk storage didn't support so we went to memory storage */
 }
 import multer from "multer";
+import DataParser from "datauri/parser.js";
+import path from "path";
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    // set the directory where uploaded files will be stored
-    cb(null, "public/uploads");
-  },
-  filename: (req, file, cb) => {
-    const fileName = file.originalname;
-    // set the name of the uploaded file
-    cb(null, fileName);
-  },
-});
+const storage = multer.memoryStorage();
 
 const upload = multer({ storage });
+const parser = new DataParser();
+
+export const formatImage = (file) => {
+  const fileExtension = path.extname(file.originalname).toString();
+  return parser.format(fileExtension, file.buffer).content;
+};
 
 export default upload;
+
+// Disk storage setup
+// import multer from "multer";
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     // set the directory where uploaded files will be stored
+//     cb(null, "public/uploads");
+//   },
+//   filename: (req, file, cb) => {
+//     const fileName = file.originalname;
+//     // set the name of the uploaded file
+//     cb(null, fileName);
+//   },
+// });
+// const upload = multer({ storage });
+// export default upload;
